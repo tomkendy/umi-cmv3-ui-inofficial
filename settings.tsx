361bd9d183f9dbccac05c25f6ab88352e8c13119
tomkendy <162161195+tomkendy@users.noreply.github.com>
2024-03-05 07:53:13 +0300

    Update settings.tsx

--- settings.tsx
+++ settings.tsx
@@ -1,7 +1,7 @@
 // Purpose: Contains texts that will be used in the UI when rendering the buttons
 // label has to be the exact same as in your candy machine config
 export const mintText = [
-  { label: "default", buttonLabel: "MINT",}
+  { label: "default", buttonLabel: "MINT", header:"MINT", desc:"MINT"}
 ];
 
 //header image in the ui. replace with your own
